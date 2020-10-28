fa9af6074f5cec050c142817e3c1f443b62d6111
a.pomosov <a.pomosov@alibaba-inc.com>
2020-10-28 23:49:19 +0800

    Billing service

---
+++ billing/src/main/java/io/github/javaasasecondlanguage/billing/BillingServer.java
@@ -0,0 +1,11 @@
+package io.github.javaasasecondlanguage.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServer {
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServer.class, args);
+    }
+}
