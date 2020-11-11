d4f024793c4e19647d3769d5338cc369cd09da47
a.pomosov <a.pomosov@alibaba-inc.com>
2020-11-11 23:47:03 +0800

    Lecture08

---
+++ lecture08/src/test/java/io/github/javaasasecondlanguage/lecture08/practice1/FixIt1.java
@@ -0,0 +1,29 @@
+package io.github.javaasasecondlanguage.lecture08.practice1;
+
+import org.junit.jupiter.api.Test;
+
+public class FixIt1 {
+    boolean isT2Running = true;
+
+    @Test
+    public void test() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            int counter = 0;
+            while (isT2Running) {
+                counter++;
+            }
+            System.out.println("Thread 1 finished. Counted up to " + counter);
+        });
+        t1.start();
+
+        Thread.sleep(100); // Sleep for a bit so that thread 1 has a chance to start
+
+        Thread t2 = new Thread(() -> {
+            isT2Running = false;
+            System.out.println("Thread 2 finished");
+        });
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
