7a43cab14035b23435f5420b5b07c0f781deb288
user-vi <50022091+user-vi@users.noreply.github.com>
2020-12-09 21:26:45 +0300

    hw3

--- homework03/build.gradle.kts
+++ homework03/build.gradle.kts
@@ -4,3 +4,53 @@
  * This is a general purpose Gradle build.
  * Learn how to create Gradle builds at https://guides.gradle.org/creating-new-gradle-builds
  */
+group = "io.github.javaasasecondlanguage"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    java
+    checkstyle
+    id("org.springframework.boot") version "2.2.0.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+enableJavaPreviewFeatures()
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+checkstyle {
+    toolVersion = "8.36.1"
+    config = resources.text.fromFile("../../config/checkstyle/checkstyle.xml")
+}
+
+tasks.withType<Checkstyle> {
+    reports {
+        xml.isEnabled = false
+        html.isEnabled = true
+    }
+}
+
+fun enableJavaPreviewFeatures() {
+    tasks.withType<JavaCompile> {
+        options.compilerArgs.add("--enable-preview")
+    }
+
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+        jvmArgs("--enable-preview")
+    }
+}
