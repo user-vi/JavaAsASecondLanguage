a9280372ee5f74b6e5a197e6ffa9102e0b7d641d
a.pomosov <a.pomosov@alibaba-inc.com>
2020-12-09 23:48:24 +0800

    Fix style

--- cloud/src/main/java/io/github/javaasasecondlanguage/cloud/CloudResource.java
+++ cloud/src/main/java/io/github/javaasasecondlanguage/cloud/CloudResource.java
@@ -1,16 +1,10 @@
 package io.github.javaasasecondlanguage.cloud;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Broken implementation of billing service
